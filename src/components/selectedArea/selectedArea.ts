0727c853377092d6ba75b38511de1706f68c23a3
youryu0212 <youryu0212@naver.com>
2022-07-17 00:57:19 +0900

    feat: 검색어 옆 x를 누르면 검색어 삭제, 리셋버튼 누르면 검색어 초기화 적용
    검색 상태가 입력되면 검색 목록을 리렌더링한다.
    검색어 옆의 x를 누르면 해당 검색어가 삭제된다.
    검색어가 삭제되면 해당 검색어에 해당하는 필터 버튼 활성화를 해제한다.
    모든 검색어 목록 가장 우측에 있는 리셋 버튼을 누르면 검색어가 초기화된다.

--- src/components/selectedArea/selectedArea.ts
+++ src/components/selectedArea/selectedArea.ts
@@ -1,9 +1,55 @@
 import { RESET_BUTTON } from 'src/assets/svg';
-import { render } from 'src/utils/dom';
+import { SEARCH_STATE_NAME } from 'src/constants/constants';
+import searchFilterContext from 'src/reducer/searchFilterProvider';
+import { innerHTML, qs, render } from 'src/utils/dom';
+import { go, map } from 'src/utils/utils';
+import createSelectedFilter from './selectedFilter/selectedFilter';
+
+const reRender = (words) =>
+  go(
+    words,
+    map(({ word }) => createSelectedFilter(word)),
+    (words) => innerHTML(qs('.selected-filter-area'), words),
+  );
+
+const resetFilterButton = () => {
+  const { dispatch } = searchFilterContext;
+  dispatch({ type: 'SEARCH_RESET', notify: [SEARCH_STATE_NAME] });
+};
+
+const removeSelectedFilter = ({ target }) => {
+  const { dispatch } = searchFilterContext;
+  const $targetFilter = target.closest('.selected-filter');
+  const { innerHTML } = qs('.word', $targetFilter);
+  dispatch({ type: 'REMOVE', words: innerHTML, notify: [SEARCH_STATE_NAME] });
+};
 
 const createSelectedArea = () => {
-  const selectedFilterArea = `<div class="selected-filter-area"></div>`;
-  const childComponents = [selectedFilterArea, RESET_BUTTON];
+  const { store, setObserver } = searchFilterContext;
+
+  const $selectedFilterArea = render({
+    tag: 'div',
+    attributes: { class: 'selected-filter-area' },
+    handler: removeSelectedFilter,
+    eventName: 'click',
+    selector: '.close-btn',
+  });
+
+  const $resetButton = render({
+    tag: 'div',
+    attributes: { class: 'reset-btn' },
+    eventName: 'click',
+    handler: resetFilterButton,
+    childComponents: RESET_BUTTON,
+  });
+
+  const childComponents = [$selectedFilterArea, $resetButton];
+
+  setObserver(SEARCH_STATE_NAME, () => {
+    const { state } = store;
+    const { words } = state;
+    reRender(words);
+  });
 
   const selectedAreaProps = {
     tag: 'div',
