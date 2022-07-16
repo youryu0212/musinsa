e8462ecbb70152d327f8d04addadbdd8c935c74e
youryu0212 <youryu0212@naver.com>
2022-07-17 00:53:41 +0900

    feat: 필터 버튼이 선택되면 검색 상태를 변경한다. (추가, 삭제)

--- src/components/filterArea/filterArea.ts
+++ src/components/filterArea/filterArea.ts
@@ -1,27 +1,86 @@
-import { SEARCH_BUTTON } from 'src/assets/svg';
-import { render } from 'src/utils/dom';
+import { SMALL_SEARCH_BUTTON } from 'src/assets/svg';
+import { SEARCH_BAR_OPEN_STATE_NAME, SEARCH_STATE_NAME } from 'src/constants/constants';
+import searchFilterContext from 'src/reducer/searchFilterProvider';
+import { qs, qsAll, render, toggleClassName } from 'src/utils/dom';
 import { go, map } from 'src/utils/utils';
 import createFilterButton from './filterButton/filterButton';
 
-const tag = '[filter area]';
+const filterButtonClassName = '.filter-btn';
 
 const tagList: string[] = ['검색', '세일상품', '단독상품', '품절포함'];
+const { store, setObserver, dispatch } = searchFilterContext;
 
-const handleClick = () => {
-  console.log(tag);
+const isSearchButton = (word) => {
+  return word === '검색';
+};
+
+const filterActiveButton = (words, wordElement) =>
+  map((wordElement) => {
+    const { innerHTML } = wordElement;
+    for (const keyword of words) {
+      const { word, fromSearchBar } = keyword;
+      if (innerHTML === word) {
+        wordElement.classList.add('active-filter');
+        return wordElement;
+      }
+
+      if (fromSearchBar && isSearchButton(innerHTML)) {
+        wordElement.classList.add('active-filter');
+        return wordElement;
+      }
+    }
+    wordElement.classList.remove('active-filter');
+    return wordElement;
+  })(wordElement);
+
+const setActiveButton = () => {
+  const { state } = store;
+  const { words } = state;
+  const filterButtons = qsAll('.filter-btn');
+
+  go(
+    filterButtons,
+    map((filterButton) => qs('.word', filterButton)),
+    (wordElement) => filterActiveButton(words, wordElement),
+  );
+};
+
+const handleClick = ({ target }) => {
+  const filterButton = target.closest(filterButtonClassName);
+  if (!filterButton) {
+    return;
+  }
+  const wordElement = qs('.word', filterButton);
+  const { innerHTML } = wordElement;
+
+  if (isSearchButton(innerHTML)) {
+    const isOpen = toggleClassName(filterButton, 'active-search-bar');
+    dispatch({ type: 'SEARCH_BAR_TOGGLE', isOpen, notify: [SEARCH_BAR_OPEN_STATE_NAME] });
+    return;
+  }
+
+  if (!wordElement.classList.contains('active-filter')) {
+    dispatch({ type: 'SEARCH', words: innerHTML, notify: [SEARCH_STATE_NAME] });
+    return;
+  }
+
+  dispatch({ type: 'REMOVE', words: innerHTML, notify: [SEARCH_STATE_NAME] });
+  return;
 };
 
 const createFilterArea = () => {
   const searchButton = render({
     tag: 'div',
     attributes: { class: 'small-search-btn' },
-    childComponents: SEARCH_BUTTON,
+    childComponents: SMALL_SEARCH_BUTTON,
   });
 
   const createButton = (keyword: string) => {
     const firstChild = `<div class="word">${keyword}</div>`;
     return keyword === '검색' ? [firstChild, searchButton] : firstChild;
   };
+
+  setObserver(SEARCH_STATE_NAME, setActiveButton);
 
   const childComponents = go(
     tagList,
@@ -35,6 +94,7 @@ const createFilterArea = () => {
     eventName: 'click',
     handler: handleClick,
     childComponents,
+    selector: '.filter-btn',
   };
   return render(filterProps);
 };
