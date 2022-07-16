8b4664f9a8b36f1b963751cac43912438ec6972d
youryu0212 <youryu0212@naver.com>
2022-07-17 08:08:42 +0900

    refactor: 상품 이미지 컴포넌트 수정 (템플릿 생성에서 컴포넌트 생성함수 기반으로)

--- src/components/product/productImg/productImg.ts
+++ src/components/product/productImg/productImg.ts
@@ -1,16 +1,30 @@
+import { CANDIDATE_IMG_URL } from 'src/constants/constants';
+import { render } from 'src/utils/dom';
 import { ProductType } from '../product';
 
 type ProductImgType = Pick<ProductType, 'imageUrl' | 'isExclusive' | 'goodsName' | 'isSoldOut'>;
 
+const handleError = ({ target }) => {
+  target.src = CANDIDATE_IMG_URL;
+};
+
 const createProductImg = ({ imageUrl, isExclusive, goodsName, isSoldOut }: ProductImgType) => {
-  const components = `
-  <div class="product-img ${isSoldOut ? 'sold-out__img' : ''}">
-    <img src="${imageUrl}" alt="${goodsName}" />
-    ${isExclusive ? `<span class="exclusive">독점</span>` : ''}
-    ${isSoldOut ? `<div class="sold-out__text">SOLD OUT</div>` : ''}
-  </div>
-  `;
-  return components;
+  const Img = render({
+    tag: 'img',
+    attributes: { class: `product-img ${isSoldOut ? 'sold-out__img' : ''}`, src: imageUrl, alt: goodsName },
+    eventName: 'error',
+    handler: handleError,
+  });
+  const Exclusive = isExclusive ? `<span class="exclusive">독점</span>` : '';
+  const SoldOut = isSoldOut ? `<div class="sold-out__text">SOLD OUT</div>` : '';
+
+  const childComponents = [Img, Exclusive, SoldOut];
+
+  return render({
+    tag: 'div',
+    attributes: { class: `product-img ${isSoldOut ? 'sold-out__img' : ''}` },
+    childComponents,
+  });
 };
 
 export default createProductImg;
